69f1f5d67fa31d8060ba4ffa5e8b52f6725cfa5f
Stoon <melsayed.se@gmail.com>
2021-02-05 00:22:25 +0200

    Added last seen function to routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 from app import app, db
 from app.forms import LoginForms, RegisterationForm
 from app.models import User
@@ -73,3 +74,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit() # no need for db.session.add() as the user added to session by invoking current_user
