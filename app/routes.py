e405b3c027d990de94b56f0b656cb98046f1989f
Stoon <melsayed.se@gmail.com>
2021-03-05 19:15:26 +0200

    Added post form to main mage

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,22 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
-from app.forms import LoginForms, RegisterationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForms, RegisterationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Mohamed'} # dummy object
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for(index))
     posts = [
         {
             'author': {'username': 'John'},
@@ -26,7 +32,7 @@ def index():
 
     # return render_template('index.html', user=user) - use this to test whether if conditions work in template engine
     # REMOVED user=user as we're using real data in DB now
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
